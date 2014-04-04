42a08b7831c19be7ff9a0aaf93f930599935d87a
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-04-04 13:24:53 +0200

    WCM-56 Add AnalyticsServlet

---
+++ hooks/analytics-hook/docroot/WEB-INF/src/com/liferay/analytics/servlet/AnalyticsServlet.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.servlet;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.messaging.MessageBusUtil;
+import com.liferay.portal.util.PortalUtil;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Eduardo Garcia
+ */
+public class AnalyticsServlet extends HttpServlet {
+
+	@Override
+	public void service(
+			HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException {
+
+		try {
+			Message message = new Message();
+
+			message.put("anonymousUserId", _getAnonymousUserId());
+
+			MessageBusUtil.sendMessage("liferay/analytics", message);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			PortalUtil.sendError(
+				HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e, request,
+				response);
+		}
+	}
+
+	private String _getAnonymousUserId() {
+		return "TEST";
+	}
+
+	private static Log _log = LogFactoryUtil.getLog(AnalyticsServlet.class);
+
+}
